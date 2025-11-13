4b544d103110d53155a5cebdcc268d30044aec40
Tinnawut Noinarai <Tinnawut.nnr@gmail.com>
2025-11-14 00:33:31 +0700

    feat: initialize Spring Boot backend

---
+++ src/main/java/com/pgad/pgadbackend/PgadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pgad.pgadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgadBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pgad/pgadbackend/PgadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgad.pgadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
